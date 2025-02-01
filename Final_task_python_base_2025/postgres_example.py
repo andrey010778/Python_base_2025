4f4ff7d6a2c7eb7980d5f0c5a8a4b2ca4e172ff6
Андрей <7kuz@mail.ru>
2025-02-01 21:57:57 +0300

    Create test file for postgres

---
+++ Final_task_python_base_2025/postgres_example.py
@@ -0,0 +1,21 @@
+import psycopg2
+from psycopg2 import sql
+conn = psycopg2.connect(dbname="metanit", user="postgres", password='admin', host='localhost')
+print("Подключение установлено")
+cursor = conn.cursor()
+
+
+# sql = 'create database metanit'
+# cursor.execute(sql)
+# print('Create database success')
+
+
+cursor.execute('create table if not exists people  (id serial primary key, name varchar(50), age integer)')
+name = input('Введите имя')
+age = int(input('Введите возрвст'))
+cursor.execute("insert into people (name, age) values (%s, %s)", name, age)
+
+conn.commit()
+
+cursor.close()
+conn.close()
