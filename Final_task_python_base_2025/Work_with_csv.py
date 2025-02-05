2032ab1ff65bf13c5dcc232e2a068705a0ff0544
andrey.kuznetsov <7kuz@mail.ru>
2025-02-05 08:26:17 +0300

    Добавлен файл для работы с csv и сам csv. Работа проверена - данные в таблицу вставлены.

--- Final_task_python_base_2025/Work_with_csv.py
+++ Final_task_python_base_2025/Work_with_csv.py
@@ -1,9 +1,33 @@
+import psycopg2
 import csv
 
-def read_from_csv():
-    file_name = "Name_age.csv"
-    with open(file_name, "r", newline="") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
-read_from_csv()
+def insert_csv_to_postgres(csv_file_path, db_config, table_name):
+    try:
+        conn = psycopg2.connect(**db_config)
+        cursor = conn.cursor()
+        print("Connection with base success")
+
+        with open(csv_file_path, "r") as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader)
+            columns = ','.join(header)
+            placeholders = ','.join(['%s']*len(header))
+            query = f"insert into {table_name} ({columns}) values ({placeholders})"
+            for row in reader:
+                cursor.execute(query, row)
+            conn.commit()
+            print('Data add success')
+    except Exception as e:
+        print(f"Error: {e}")
+
+        if conn:
+            cursor.close()
+            conn.close()
+            print("Connection with database closed ")
+
+
+if __name__ == "__main__":
+    db_config = {"host": "localhost", "dbname": "t_input_test", "user": "postgres", "password":"admin", "port": 5432}
+    csv_file_path = "Name_age.csv"
+    table_name = "t_input_test"
+    insert_csv_to_postgres(csv_file_path, db_config, table_name)
